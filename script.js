e2ef48169417b5ca9ea969640a08ba7f8c43f6f8
Krzysztof Starkowicz <38637017+Bysiek5@users.noreply.github.com>
2018-06-27 00:05:56 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ var Person = function(name, yearOfBirth, job) {
 }
 
 Person.prototype.calculateAge = function() {
-        console.log(2016 - this.yearOfBirth);
+        console.log(2018 - this.yearOfBirth);
     }
 
 Person.prototype.lastName = 'Smith';
@@ -42,9 +42,10 @@ console.log(mark.lastName);
 
 
 //Object.create
+/*
 var personProto = {
     calculateAge: function() {
-        console.log(2016 - this.year.OfBirth);
+        console.log(2018 - this.year.OfBirth);
     }
 };
 
@@ -59,8 +60,44 @@ var jane = Object.create(personProto,
     yearOfBirth: { value: 1969 },
     job: { value: 'designer' }
 })
+*/
 
 
+// Primitives vs objects
+// Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+// Objects
+var obj1 = {
+    name: 'John',
+    age: 26
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+// Functions
+
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
+};
+
+function change(a, b) {
+    a = 30;
+    b.city = 'San Francisco';
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city); 
 
 
 
