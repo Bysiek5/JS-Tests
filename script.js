4619f96d2e9fc80665bb68e98e5f341a38d47478
Krzysztof Starkowicz <38637017+Bysiek5@users.noreply.github.com>
2018-06-29 18:41:14 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -181,6 +181,7 @@ function game() {
 }
 game();
 */
+/*
 (function () {
     var score = Math.random() * 10;
     console.log(score >= 5);
@@ -192,17 +193,256 @@ game();
     var score = Math.random() * 10;
     console.log(score >= 5 - goodLuck);
 })(5);
+*/
+
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+// Closures
+/*
+function retirement(retirementAge) {
+    var a = ' years left until retirement.';
+    return function(yearOfBirth) {
+        var age = 2018 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+
+
+retirementGermany(1990);
+retirementUS(1990);
+retirementIceland(1990);
+*/
+
+//retirement(66)(1990);
+
+/*
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you tech, ' + name + '?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+*/
+/*
+function interviewQuestion(job) {
+    return function(name) {
+        if (job === 'designer') {
+            console.log(name + ', can you please explain what UX design is?');
+        } else if (job === 'teacher') {
+            console.log('What subject do you tech, ' + name + '?');    
+        } else {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+interviewQuestion('teacher')('John');
+*/
+
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+// Bind, call and apply
+/*
+var john = {
+    name: 'John',
+    age: 26,
+    job: 'techer',
+    presentation: function(style, timeOfDay) {
+        if (style === 'formal') {
+            console.log('Good ' + timeOfDay + ', Ladies and gentelmen! I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+        } else if (style === 'friendly') {
+            console.log('Hey! What\'s up? I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+        }
+    }
+};
+
+var emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'designer'
+};
+
+john.presentation('formal', 'morning');
+
+john.presentation.call(emily, 'friendly', 'afternoon');
+
+//john.presentation.apply(emily, ['friendly', 'afternoon']);
+
+var johnFriendly = john.presentation.bind(john, 'friendly');
+
+johnFriendly('morning');
+johnFriendly('night');
+
+
+var emilyFormal = john.presentation.bind(emily, 'formal');
+
+emilyFormal('evening');
 
 
 
 
 
 
+var years = [1990, 1965, 1937, 2005, 1998];
 
+function arrayCalc(arr, fn) {
+    var arrResult = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrResult.push(fn(arr[i]));
+    }
+    return arrResult;
+}
 
+function calculateAge(element) {
+    return 2018 - element;
+}
 
+function isFullAge(limit, element) {
+    return element >= limit;
+}
 
+var ages = arrayCalc(years, calculateAge);
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+console.log(ages);
+console.log(fullJapan);
+*/
 
+/*
+(function() {
+    function Question(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
 
+    Question.prototype.displayQuestion = function() {
+        console.log(this.question);
 
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' + this.answers[i]);
+        }
+    }
 
+    Question.prototype.checkAnswer = function(ans) {
+        if (ans === this.correct) {
+            console.log('Correct answer!');
+        } else {
+            console.log('Wrong answer. Try again!');
+        }
+    }
+
+    var q1 = new Question('Is JavaScript the coolest programming language in the world?', 
+                          ['Yes', 'No'], 
+                          0);
+
+    var q2 = new Question('What is the name of this course\'s teacher?', 
+                          ['John', 'Michael', 'Jonas'], 
+                          2);
+
+    var q3 =new Question('What does best describe coding?', 
+                        ['Boring', 'Hard', 'Fun', 'Tedious'],
+                        2);
+
+    var questions = [q1, q2, q3];
+
+    var n = Math.floor(Math.random() * questions.length);
+
+    questions[n].displayQuestion();
+
+    var answer = parseInt (prompt('Please select the correct answer.'));
+
+    questions[n].checkAnswer(answer);
+})();
+*/
+
+(function() {
+    function Question(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
+
+    Question.prototype.displayQuestion = function() {
+        console.log(this.question);
+
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' + this.answers[i]);
+        }
+    }
+
+    Question.prototype.checkAnswer = function(ans, callback) {
+        var sc;
+        if (ans === this.correct) {
+            console.log('Correct answer!');
+            sc = callback(true);
+        } else {
+            console.log('Wrong answer. Try again!');
+            
+            sc = callback(false);
+        }
+        
+        this.displayScore(sc);
+    }
+
+    Question.prototype.displayScore = function(score) {
+        console.log('Your current score is: ' +score);
+        console.log('----------------------------------------');
+    }
+    
+    var q1 = new Question('Is JavaScript the coolest programming language in the world?', 
+                          ['Yes', 'No'], 
+                          0);
+
+    var q2 = new Question('What is the name of this course\'s teacher?', 
+                          ['John', 'Michael', 'Jonas'], 
+                          2);
+
+    var q3 =new Question('What does best describe coding?', 
+                        ['Boring', 'Hard', 'Fun', 'Tedious'],
+                        2);
+    
+    var questions = [q1, q2, q3];
+    
+    function score() {
+        var sc = 0;
+        return function(correct) {
+            if (correct) {
+                sc++;
+            } return sc;
+        }
+    }
+    
+    var keepScore = score();
+    
+    function nextQuestion() {
+
+        var n = Math.floor(Math.random() * questions.length);
+
+        questions[n].displayQuestion();
+
+        var answer = prompt('Please select the correct answer.');
+
+        if(answer !== 'exit') {
+            questions[n].checkAnswer(parseInt(answer), keepScore);
+
+            nextQuestion();
+        }
+    }
+
+    nextQuestion();
+    
+})();
